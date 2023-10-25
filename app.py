ec0ef8d856917e482dac7119e6fc0db4059907c3
dij-kstra <140788057+dij-kstra@users.noreply.github.com>
2023-10-25 12:22:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,39 +2,69 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import cv2
-from tensorflow.keras.models import load_model
+from keras.models import model_from_json
 
 # Load the trained model
-model = load_model('facial_expression_model.h5')
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+
+model.load_weights('model_weights.h5')
+
+# Load the Haar Cascade classifier for face detection
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Define emotion labels
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 # Streamlit app
+st.set_page_config(page_title="Facial Emotion Recognition", page_icon="✅", initial_sidebar_state="expanded")
+
 st.title('Facial Emotion Recognition')
-st.write('Upload a cropped Image of a face of single person')
+st.write('Upload an Image. Faces will be detected and their emotions will be predicted')
 
 # Upload image through Streamlit UI
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     try:
-        # Read and preprocess the uploaded image
+        # Read the uploaded image
         image = Image.open(uploaded_file)
-        image = image.convert('L')  # Convert to grayscale
-        image = image.resize((48, 48))  # Resize to model's input size
-        image = np.array(image) / 255.0  # Normalize pixel values
+        img_array = np.array(image)
 
-        # Ensure the image has the correct shape (batch_size, height, width, channels)
-        image = np.expand_dims(image, axis=0)  # Add batch dimension
-        image = np.expand_dims(image, axis=-1)  # Add channel dimension
+        # Convert to grayscale for face detection
+        gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
 
-        # Make prediction using the model
-        prediction = model.predict(image)
-        predicted_label = emotion_labels[np.argmax(prediction)]
+        # Detect faces using the Haar Cascade classifier
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-        # Display the uploaded image and prediction
-        st.image(image[0, :, :, 0], caption='Uploaded Image', use_column_width=True)
-        st.write(f"Predicted Emotion: {predicted_label}")
+        # Create a copy of the image to draw rectangles on
+        image_with_rectangles = np.copy(img_array)
+
+        # Process each detected face
+        for (x, y, w, h) in faces:
+            # Draw a rectangle around each face
+            cv2.rectangle(image_with_rectangles, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+            # Extract and preprocess the detected face for emotion prediction
+            detected_face = gray[y:y + h, x:x + w]
+            detected_face = cv2.resize(detected_face, (48, 48))
+            detected_face = detected_face / 255.0
+            detected_face = np.expand_dims(detected_face, axis=0)
+            detected_face = np.expand_dims(detected_face, axis=-1)
+
+            # Make emotion prediction using the model
+            emotion_prediction = model.predict(detected_face)
+            predicted_label = emotion_labels[np.argmax(emotion_prediction)]
+
+            # Add the predicted emotion label near the rectangle
+            cv2.putText(image_with_rectangles, predicted_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+        # Display the color image with rectangles and predictions
+        st.image(image_with_rectangles, caption='Uploaded Image with Emotion Predictions', use_column_width=True)
+
     except Exception as e:
         st.error(f"Error processing image: {e}")
+
+
